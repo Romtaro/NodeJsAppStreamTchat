309b8d0a79bf23a7eef7dbb04d14ffac0ad5ba67
draden05 <rodolphe.lebrun@ynov.com>
2017-05-19 16:28:46 +0200

    dd

--- middlewares/flash.js
+++ middlewares/flash.js
@@ -13,13 +13,3 @@ module.exports = function (request, response, next) {
   }
   next()
 }
-
-function authenticationMiddleware () {
-    return function (req, res, next) {
-        if (req.isAuthenticated()) {
-            return next()
-        }
-        res.redirect('/')
-
-    }
-}
