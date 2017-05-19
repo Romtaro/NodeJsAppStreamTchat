84403ea4ad6cf6beb179ae6b2cd0745ee01ce26e
draden05 <rodolphe.lebrun@ynov.com>
2017-05-19 13:56:14 +0200

    up (rodz) fix route css etc..

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let express = require('express')
+var path = require('path')
 let app = express()
 let bodyParser = require('body-parser')
 let session = require('express-session')
@@ -9,7 +10,7 @@ process.env.NODE_ENV="production";
 app.set('view engine', 'ejs')
 
 // Middleware
-app.use('/assets', express.static('public'))
+app.use(express.static(path.join(__dirname, 'www')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(session({
